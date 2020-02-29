12bbc4e47887ed59f145d84f2d580f4678f51b05
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2020-02-29 18:14:36 +0000

    hieradata y manifests

---
+++ manifests/app.pp
@@ -0,0 +1,47 @@
+node 'node1' {
+
+    #include base
+
+    class {'::tomcat':
+        xms             => '60m',
+        xmx             => '120m',
+        user            => 'tomcat',
+        group           => 'tomcat',
+        service_state   => 'running'
+    }
+    
+    tomcat::deploy { "sysfoo":
+        deploy_url      =>  'https://42-243470015-gh.circle-artifacts.com/0/tmp/artifacts/sysfoo.war',
+        checksum_value  => '09af421c3f483c615ff587141a2806ab',
+    }
+
+}
+
+node default {
+
+    notify{"checkpoint_1":
+
+        message => " *** DEFAULT BLOCK *** "
+    
+    }
+
+}
+
+node 'node2' {
+
+    #include base
+
+    class {'::tomcat':
+        xms             => '70m',
+        xmx             => '130m',
+        user            => 'tomcat',
+        group           => 'tomcat',
+        service_state   => 'running'
+    }
+    
+    tomcat::deploy { "sysfoo":
+        deploy_url      =>  'https://42-243470015-gh.circle-artifacts.com/0/tmp/artifacts/sysfoo.war',
+        checksum_value  => '09af421c3f483c615ff587141a2806ab',
+    }
+
+}
