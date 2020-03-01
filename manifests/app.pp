9ea1c441f5c2c19fcd88c51aa4ae2279d2f2540f
Sp4ceCowb0y <rfiguerasdegea@outlook.com>
2020-03-01 13:22:57 +0000

    dev

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,47 +1,18 @@
-node 'node1' {
+node 'node3' {
 
-    include base
-
-    class {'::tomcat':
-        xms             => '60m',
-        xmx             => '120m',
-        user            => 'tomcat',
-        group           => 'tomcat',
-        service_state   => 'running'
-    }
+  include base
+  include tomcat
+  class {'::tomcat':
+      xms             => '60m',
+      xmx             => '120m',
+      user            => 'tomcat',
+      group           => 'tomcat',
+      service_state   => 'running'
+  }
     
-    #tomcat::deploy { "sysfoo":
-    #    deploy_url      =>  'https://42-243470015-gh.circle-artifacts.com/0/tmp/artifacts/sysfoo.war',
-    #    checksum_value  => '09af421c3f483c615ff587141a2806ab',
-    #}
-
-}
-
-node default {
-
-    notify{"checkpoint_1":
-
-        message => " *** DEFAULT BLOCK *** "
-    
-    }
-
-}
-
-node 'node2' {
-
-    include base
-
-    class {'::tomcat':
-        xms             => '70m',
-        xmx             => '130m',
-        user            => 'tomcat',
-        group           => 'tomcat',
-        service_state   => 'running'
-    }
-    
-    #tomcat::deploy { "sysfoo":
-    #    deploy_url      =>  'https://42-243470015-gh.circle-artifacts.com/0/tmp/artifacts/sysfoo.war',
-    #    checksum_value  => '09af421c3f483c615ff587141a2806ab',
-    #}
+  tomcat::deploy { "sysfoo":
+    deploy_url      => 'https://42-243470015-gh.circle-artifacts.com/0/tmp/artifacts/sysfoo.war',
+    checksum_value  => '09af421c3f483c615ff587141a2806ab',
+  }
 
 }
